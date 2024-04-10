932a376d3dc9943afc53506e0976d22858147b14
Efren <50155831+EfrenAS@users.noreply.github.com>
2024-04-09 22:31:53 -0500

    '

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import icon from 'astro-icon';
 export default defineConfig({
   integrations: [icon()],
   site: 'https://EfrenAS.github.io',
-  base: 'lostrespotrilloseventcenter',
+  base: '/lostrespotrilloseventcenter',
 });
