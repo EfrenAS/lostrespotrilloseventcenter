a9c0deb860a72b1d0c8a523c21a4e4ecec559393
Efren <50155831+EfrenAS@users.noreply.github.com>
2024-04-10 19:24:30 -0500

    [Updated] Lines for github pages deploy was remove

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,4 @@ import icon from 'astro-icon';
 // https://astro.build/config
 export default defineConfig({
   integrations: [icon()],
-  site: 'https://EfrenAS.github.io',
-  base: '/lostrespotrilloseventcenter',
 });
